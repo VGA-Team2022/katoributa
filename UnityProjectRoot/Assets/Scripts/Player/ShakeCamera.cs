5bf53f883acb950a33c61a9640769c19325cb387
NagasawaYusei <curga3013@gmail.com>
2022-12-07 14:21:50 +0900

    カメラをシェイクさせる機能を追加

---
+++ UnityProjectRoot/Assets/Scripts/Player/ShakeCamera.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using Cinemachine;
+
+public class ShakeCamera : MonoBehaviour
+{
+    public static ShakeCamera Instance;
+    CinemachineImpulseSource _source;
+    [Tooltip("U“®‹­“xAƒQ[ƒ€’†‚Í•Ï‚¦‚ê‚È‚¢"), SerializeField] float _shakeGain;
+
+    void Awake()
+    {
+        if(!Instance)
+        {
+            Instance = this;
+            return;
+        }
+
+        Destroy(gameObject);
+    }
+    void Start()
+    {
+        _source = GetComponent<Cinemachine.CinemachineImpulseSource>();
+        _source.m_ImpulseDefinition.m_AmplitudeGain = _shakeGain;
+        _source.m_ImpulseDefinition.m_FrequencyGain = _shakeGain;
+    }
+
+    public void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.I))
+        {
+            Debug.Log("“Ç‚ñ‚¾");
+            ShakeMethod();
+        }
+    }
+
+    public void ShakeMethod()
+    {
+        _source.GenerateImpulse();
+    }
+}
