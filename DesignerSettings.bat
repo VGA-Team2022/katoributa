2fe887945145b085b3417ab5c04f88809e2ce908
toya_kazuya <mimikaumi@gmail.com>
2022-12-19 10:43:55 +0900

    バッチ変更

--- DesignerSettings.bat
+++ DesignerSettings.bat
@@ -1,12 +1,9 @@
-rem //	DesignerSettingsBatch
-
 @echo on
 cd %~dp0
 set root=%~dp0
 set main=UnityProjectRoot
 set project=UnityArtProject
 
-rem ファイルの参照とコピー
 xcopy /e /y %root%%main%\Assets %root%%project%\Assets
 xcopy /e /y %root%%main%\Packages %root%%project%\Packages
 xcopy /e /y %root%%main%\ProjectSettings %root%%project%\ProjectSettings
