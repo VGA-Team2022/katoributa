32da67d91d3aafee6c9dcf4c64ecd08fa2a18378
toya_kazuya <mimikaumi@gmail.com>
2022-12-19 10:47:39 +0900

    バッチ変更

--- ProgrammerJoint.bat
+++ ProgrammerJoint.bat
@@ -1,12 +1,9 @@
-rem //	DesignerSettingsBatch
-
 @echo on
 cd %~dp0
 set root=%~dp0
 set main=UnityProjectRoot
 set project=UnityArtProject
 
-rem ファイルの参照とコピー
 xcopy /e /y %root%%project%\Assets\Master %root%%main%\Assets\Model\Master
 
 pause
