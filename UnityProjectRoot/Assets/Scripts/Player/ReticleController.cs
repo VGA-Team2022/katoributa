1038ff7e46def8a5e2d69c0e6e3ec6052e7d89a8
K-Koya <action.gamer.k@gmail.com>
2023-01-13 17:46:43 +0900

    上がったプレハブ等の変更をもとにメインのシーンに適用した

--- UnityProjectRoot/Assets/Scripts/Player/ReticleController.cs
+++ UnityProjectRoot/Assets/Scripts/Player/ReticleController.cs
@@ -9,6 +9,7 @@ public class ReticleController : MonoBehaviour
     //[SerializeField] RectTransform _uiCanvasRectTrans;
     [SerializeField] float _length = 50;
     [SerializeField] Transform _reticleTransform;
+    [SerializeField] LayerMask _onReticle;
     //[SerializeField] Camera _camera;
 
     void Awake()
@@ -20,7 +21,7 @@ public class ReticleController : MonoBehaviour
     {
         Debug.DrawRay(_muzzleTransform.position, _muzzleTransform.forward * _length, Color.red);
         RaycastHit hit;
-        if (Physics.Raycast(_muzzleTransform.position + new Vector3(0, 0.741f, 0), _muzzleTransform.forward, out hit, _length))
+        if (Physics.Raycast(_muzzleTransform.position + new Vector3(0, 0.741f, 0), _muzzleTransform.forward, out hit, _length, _onReticle))
         {
             _reticleTransform.position = hit.point;
             _reticleTransform.rotation = _muzzleTransform.rotation;
